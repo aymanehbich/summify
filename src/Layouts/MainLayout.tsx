cdb479f72fbb503edeb0db95d10e6699b7b7cdc3
hbich aymane <aymangame077@gmail.com>
2025-03-15 03:05:31 +0000

    feat: Add MainLayout component with theme toggle and GitHub link

---
+++ src/Layouts/MainLayout.tsx
@@ -0,0 +1,62 @@
+import React, { useCallback } from "react";
+import { Link, Outlet } from "react-router-dom";
+import {
+  ActionIcon,
+  AppShell,
+  Group,
+  UnstyledButton,
+  useComputedColorScheme,
+  useMantineColorScheme,
+} from "@mantine/core";
+import { CustomLogo } from "@/components/Logo/CustomLogo";
+import classes from "./MainLayout.module.css";
+import { IconMoon, IconSun } from "@tabler/icons-react";
+import { GithubIcon2 } from "@/components/Icons/GithubIcon2";
+const MainLayout: React.FC = () => {
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light", {
+    getInitialValueInEffect: true,
+  });
+  const toggleColorScheme = useCallback(() => {
+    setColorScheme(computedColorScheme === "light" ? "dark" : "light");
+  }, [computedColorScheme, setColorScheme]);
+  return (
+    <AppShell padding="md" header={{ height: 60 }}>
+      <AppShell.Header className={classes.header}>
+        <Group h="100%" px="md" justify="space-between">
+          <UnstyledButton component={Link} to={"/"}>
+            <CustomLogo type="mark" size={44} />
+          </UnstyledButton>
+          <Group gap="sm">
+            <ActionIcon
+              variant="default"
+              size={35}
+              radius="md"
+              aria-label="Visite Github"
+              component={Link}
+              to="https://github.com/aymanehbich/Summify"
+              target="_blank"
+            >
+              <GithubIcon2 size={22} />
+            </ActionIcon>
+            <ActionIcon
+              onClick={toggleColorScheme}
+              variant="default"
+              size={35}
+              radius="md"
+              aria-label="Toggle color scheme"
+            >
+              <IconSun className={classes.light} size={22} stroke={1.5} />
+              <IconMoon className={classes.dark} size={22} stroke={1.5} />
+            </ActionIcon>
+          </Group>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Main className={classes.main}>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
+  );
+};
+
+export default MainLayout;
