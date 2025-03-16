dfef93e9da503ceb7bb68f5d6f843d8976276b3b
hbich aymane <aymangame077@gmail.com>
2025-03-16 03:05:35 +0000

    chore: configure Redux store with articleApi slice
    - Set up Redux store using configureStore from Redux Toolkit.
    - Add rticleApi slice to the store's reducer.
    - Include rticleApi.middleware to enable RTK Query functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import MainLayout from "./Layouts/MainLayout";
+import { Notifications } from "@mantine/notifications";
+// import { Hero } from "./components/Hero/Hero";
+// import React, { Suspense } from "react";
+import Hero from "./components/Hero/Hero";
+// import Hero from "./components/Hero/Hero";
+// import React, { Suspense } from "react";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  // const Hero = React.lazy(() => import("./components/Hero/Hero"));
+  return (
+    <MantineProvider theme={theme}>
+      <Notifications />
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<MainLayout />}>
+            <Route
+              path="/"
+              element={
+                <Hero />
+              }
+            />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+          {/* <Route path="/test" element={<DeepSeekChat />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  );
 }
