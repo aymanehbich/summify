f58fed7bfc7ef55da4a817733f8ef5031eaf5c76
hbich aymane <aymangame077@gmail.com>
2025-06-17 12:56:06 +0100

    chore: remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,9 @@ import { theme } from "./theme";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import MainLayout from "./Layouts/MainLayout";
 import { Notifications } from "@mantine/notifications";
-// import { Hero } from "./components/Hero/Hero";
-// import React, { Suspense } from "react";
 import Hero from "./components/Hero/Hero";
-import DeepSeekChat from "./components/tests/DeepSeekChat";
-// import Hero from "./components/Hero/Hero";
-// import React, { Suspense } from "react";
 
 export default function App() {
-  // const Hero = React.lazy(() => import("./components/Hero/Hero"));
   return (
     <MantineProvider theme={theme}>
       <Notifications />
